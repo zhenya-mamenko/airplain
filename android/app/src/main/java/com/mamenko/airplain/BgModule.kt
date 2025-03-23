20238e0e0eb779f217ffb2cf33446ed463cea6df
Zhenya Mamenko <mamenko@gmail.com>
2025-03-23 23:50:09 +0400

    [FIX] Fixed cached folder case

---
+++ android/app/src/main/java/com/mamenko/airplain/BgModule.kt
@@ -0,0 +1,40 @@
+package com.mamenko.airplain
+
+import com.facebook.react.bridge.*
+import android.content.Context
+import android.content.Intent
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.os.SystemClock
+import android.util.Log
+
+class AirPlainBgModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "AirPlainBgModule"
+    }
+
+    @ReactMethod
+    fun startBackgroundTask() {
+        val context = reactApplicationContext
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, BgReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        Log.d("AirPlain", "startBackgroundTask")
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            SystemClock.elapsedRealtime() + 60000,
+            pendingIntent
+        )
+    }
+
+    @ReactMethod
+    fun stopBackgroundTask() {
+        val context = reactApplicationContext
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, BgReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        Log.d("AirPlain", "stopBackgroundTask")
+        alarmManager.cancel(pendingIntent)
+    }
+}
